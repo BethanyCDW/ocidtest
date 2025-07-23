593688f5f31f8c110a314a580945b88120cbbbe5
Bethany Baldi <93666167+BethanyCDW@users.noreply.github.com>
2025-07-23 16:37:54 -0400

    Add IAM role for EKS namespace provisioning

--- main.tf
+++ main.tf
@@ -1,17 +1,18 @@
-#module "oicd" {
-  #source = "./modules/oicd"
-#}
+module "oicd" {
+  source = "./modules/oicd"
+}
 module "ec2" {
   source          = "./modules/ec2"
   role_name       = var.role_name
   trusted_entities = var.trusted_entities
 }
 
-#module "eks" {
-  #source            = "./modules/eks"
-  #region            = "us-east-1"
-  #cluster_name      = "my-eks-cluster"
-  #oidc_provider_arn = module.oicd.oidc_provider_arn
-  #oidc_subject      = module.oicd.oidc_subject
-  #eks_node_role_arn = "arn:aws:iam::755213274221:role/EKSNodeRole"
-#}
+module "eks_namespace_role" {
+  source                 = "./modules/eks"
+  role_name              = "eks-namespace-role"
+  oidc_provider_url = module.oicd.oidc_provider_url
+  oidc_provider_arn = module.oicd.oidc_provider_arn
+  eks_cluster_arn         = "arn:aws:eks:us-east-2:755213274221:cluster/my-eks-cluster"
+  s3_bucket_arn          = "arn:aws:s3:::bb-oicd-test"
+  service_account_subject = "system:serviceaccount:kube-system:my-sa"
+}
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,5 +1,29 @@
-variable "cluster_name" {}
-variable "region" {}
-variable "oidc_provider_arn" {}
-variable "oidc_subject" {}
-variable "eks_node_role_arn" {} # Optional if you're managing nodes separately
+variable "role_name" {
+  description = "Name of the IAM role"
+  type        = string
+}
+
+variable "oidc_provider_url" {
+  description = "OIDC provider URL for the EKS cluster"
+  type        = string
+}
+
+variable "oidc_provider_arn" {
+  description = "OIDC provider ARN"
+  type        = string
+}
+
+variable "eks_cluster_arn" {
+  description = "ARN of the EKS cluster"
+  type        = string
+}
+
+variable "s3_bucket_arn" {
+  description = "ARN of the S3 bucket to store Terraform state"
+  type        = string
+}
+
+variable "service_account_subject" {
+  description = "OIDC subject format: system:serviceaccount:<namespace>:<serviceaccount-name>"
+  type        = string
+}
